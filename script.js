b946f3b19537a1a325ed396443ee29ea52404e5b
Kiran Padole <124488404+PadoleKiran@users.noreply.github.com>
2026-02-21 12:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+ const codes = document.querySelectorAll(".code");
+
+    codes[0].focus();
+
+    codes.forEach((input, index) => {
+
+        input.addEventListener("input", (e) => {
+            const value = e.target.value;
+
+            // Allow only digits
+            if (!/^\d$/.test(value)) {
+                e.target.value = "";
+                return;
+            }
+
+            // Move to next input
+            if (index < codes.length - 1) {
+                codes[index + 1].focus();
+            }
+        });
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace") {
+
+                if (input.value === "" && index > 0) {
+                    codes[index - 1].value = "";
+                    codes[index - 1].focus();
+                    e.preventDefault();
+                }
+            }
+        });
+
+    });
