e186c4040649547b59a8762a229634f9f069f3e5
Kiran Padole <124488404+PadoleKiran@users.noreply.github.com>
2026-02-21 12:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 //your JS code here. If required.
- const codes = document.querySelectorAll(".code");
+  const inputs = document.querySelectorAll(".code");
 
-    codes[0].focus();
+  inputs[0].focus();
 
-    codes.forEach((input, index) => {
+  inputs.forEach((input, index) => {
 
-        input.addEventListener("input", (e) => {
-            const value = e.target.value;
+    input.addEventListener("input", (e) => {
+      if (!/^\d$/.test(e.target.value)) {
+        e.target.value = "";
+        return;
+      }
 
-            // Allow only digits
-            if (!/^\d$/.test(value)) {
-                e.target.value = "";
-                return;
-            }
-
-            // Move to next input
-            if (index < codes.length - 1) {
-                codes[index + 1].focus();
-            }
-        });
-
-        input.addEventListener("keydown", (e) => {
-            if (e.key === "Backspace") {
-
-                if (input.value === "" && index > 0) {
-                    codes[index - 1].value = "";
-                    codes[index - 1].focus();
-                    e.preventDefault();
-                }
-            }
-        });
-
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
     });
+
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace") {
+
+        if (input.value === "" && index > 0) {
+          inputs[index - 1].focus();
+        }
+      }
+    });
+
+  });
